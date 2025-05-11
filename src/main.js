19cc991ef24ff54815175d2b8c5a65ae6535d0a9
oseembiya <ozeembiya@gmail.com>
2025-05-11 16:23:07 +0100

    iji

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./assets/main.css";
-import "./assets/fontawesome.css";
 
-import { createApp } from "vue";
+// Use import maps or CDN for Vue in development
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
