7f393488c5745575a225f1fbed0f632049aa456f
oseembiya <ozeembiya@gmail.com>
2025-05-11 10:17:10 +0100

    dd

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
       // entry for the page
       entry: "src/main.js",
       // the source template
-      template: "src/index.html",
+      template: "index.html",
       // output as dist/index.html
       filename: "index.html",
       // when using title option, template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
