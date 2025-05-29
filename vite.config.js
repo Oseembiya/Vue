99986a2037673a133da03e6373cc26e730f9c33d
oseembiya <ozeembiya@gmail.com>
2025-05-29 15:21:41 +0100

    frfr

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Vue/",
+  base: process.env.NODE_ENV === "production" ? "/Vue/" : "/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
