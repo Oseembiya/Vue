1e8dcfbf259c51d6779a04ea76cd53e2339c96b0
oseembiya <ozeembiya@gmail.com>
2025-05-29 15:25:43 +0100

    xsx

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "./assets/main.css";
 
-// Use the production build of Vue from node_modules
-import { createApp } from "vue";
+// Use Vue from CDN
+const { createApp } = Vue;
 import App from "./App.vue";
 
 createApp(App).mount("#app");
